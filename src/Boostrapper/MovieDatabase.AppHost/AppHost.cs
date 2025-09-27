73184df5edfd1d80d2721fb3b6b93b0da5b50517
Daniel Olczyk <contact@dolczyk.rocks>
2025-09-27 14:47:18 +0200

    Setup CosmosDB emulator for development environment

--- src/Boostrapper/MovieDatabase.AppHost/AppHost.cs
+++ src/Boostrapper/MovieDatabase.AppHost/AppHost.cs
@@ -1,8 +1,16 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
+var isDevelopment = builder.Environment.IsDevelopment();
 var cosmos = builder.AddAzureCosmosDB("movies-db-cosmos");
-var moviesDb = cosmos.AddCosmosDatabase("movie-db");
-cosmos.RunAsPreviewEmulator();
+// var moviesDb = cosmos.AddCosmosDatabase("movie-db");
+
+if (isDevelopment)
+{
+    cosmos.RunAsPreviewEmulator();
+}
+
 
 var apiService = builder.AddProject<Projects.MovieDatabase_Api>("movies-db-api");
 
